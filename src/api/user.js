90cca31ead8344544512f7119ea7e66f01ad9fa1
大猫猫 <xianglvs@gmail.com>
2019-08-15 23:52:07 +0800

    优化代码

--- src/api/user.js
+++ src/api/user.js
@@ -1,39 +1,35 @@
 import request from '@/utils/request'
 
-export function login(data) {
-  return new Promise((resolve, reject) => {
-    request({
+export async function login(data) {
+  try {
+    const res = await request({
       url: '/api/user/login',
       method: 'post',
       data
-    }).then(res => {
-      if (res.code === 0) {
-        return request({
-          url: '/api/user/token',
-          method: 'post',
-          data: { ticket: res.data.ticket }
-        }).then((res1) => {
-          if (res1.code !== 0) {
-            resolve(res1)
-            return
-          }
-          res.data.token = res1.data.token
-          resolve(res)
-        })
-      } else {
-        resolve(res)
-      }
-    }).catch(e => {
-      reject(e)
     })
-  })
+    if (res.code !== 0) {
+      return Promise.resolve(res)
+    }
+    const res1 = await request({
+      url: '/api/user/token',
+      method: 'post',
+      data: {
+        ticket: res.data.ticket
+      }
+    })
+    return Promise.resolve(res1)
+  } catch (e) {
+    return Promise.reject(e)
+  }
 }
 
 export function getInfo(token) {
   return request({
     url: '/user/info',
     method: 'get',
-    params: { token }
+    params: {
+      token
+    }
   })
 }
 
