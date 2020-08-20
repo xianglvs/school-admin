796cd5bfe8eb21d3b633b96ff46d097687e9e6d2
shdk_kyz <shdk_kyz@chint.com>
2020-08-20 14:03:09 +0800

    修改

--- vue.config.js
+++ vue.config.js
@@ -41,9 +41,9 @@ module.exports = {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API
       ]: {
-        // target: `http://127.0.0.1:${port}/mock`,
+        target: `http://127.0.0.1:${port}/mock`,
         // target: `http://120.78.133.215:9998`,
-        target: `http://127.0.0.1:9998`,
+        // target: `http://127.0.0.1:9998`,
         changeOrigin: true,
         pathRewrite: {
           ["^" + process.env.VUE_APP_BASE_API]: ""
